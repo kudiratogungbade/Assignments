726566322cdda1279b7f8cde6ae430d34476417d
Kudirat Ogungbade <114508768+kudiratogungbade@users.noreply.github.com>
2022-10-29 10:54:06 +0100

    Create index.js

---
+++ Portfolio & server/index.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
+const server = http.createServer((req, res) => {
+console.log(req.url, req.method);
+console.log(req.url)
+//set header content type
+res.setHeader('Content-Type', 'text/html');
+let route = './public/';
+switch (req.url) {
+case '/home':
+route += 'index.html';
+break;
+case '/contact':
+route += 'contact.html';
+break;
+case '/about':
+route += 'about.html';
+break;
+default:
+route += '404.html'
+break;
+}
+fs.readFile(route, (err, data) => {
+if (err) {
+console.log(err)
+res.end()
+} else {
+res.end(data);
+}
+})
+})
+server.listen(3000, 'localhost', () => {
+console.log(`listening on  http://localhost:${port}/home`)
+});
